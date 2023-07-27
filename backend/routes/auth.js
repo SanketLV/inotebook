2b2675ea0a3196181fdeeadc3329903f0ab177ae
SanketLakhani <sanketlakhani80@gmail.com>
2023-07-27 19:05:25 +0530

    adding hash to secure password and send auth_Token to user.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "Sanketisagood$boy";
 
 //Create a User using: POST "/auth/createuser". No Login requires...
 router.post(
@@ -28,14 +32,27 @@ router.post(
           .status(400)
           .json({ error: "Sorry!, an user with same email already exist." });
       }
+      // Create salt and adding in password
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
 
       // Create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      console.log(authToken);
+
+      // res.json(user);
+      res.json({ authToken });
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Some Error occured");
